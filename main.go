ad2599b6491dff5f18193c3063f297909df36853
wakuwaku3 <takushi_119@live.jp>
2020-02-15 19:29:20 +0000

    mod

--- main.go
+++ main.go
@@ -19,12 +19,12 @@ func main() {
 	server := grpc.NewServer()
 	catService := &MyCatService{}
 	cat.RegisterCatServer(server, catService)
-	log.Println("serve gRPC http://127.0.0.1:8080")
+	log.Println("serve gRPC! http://127.0.0.1:8080")
 	log.Fatal(server.Serve(listenPort))
 }
 
 func serveDocument() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "./cat/index.html") })
-	log.Println("serve document http://127.0.0.1:52300")
+	log.Println("serve document! http://127.0.0.1:52300")
 	log.Fatal(http.ListenAndServe(":52300", nil))
 }
